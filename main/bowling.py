aac7fe4a6b2720347ee9be490cba15635219ef3a
messerli-wallace <jjmw1058@gmail.com>
2024-03-22 21:02:26 -0500

    Finished Bowling Kata for conditions provided

--- main/bowling.py
+++ main/bowling.py
@@ -1,35 +1,47 @@
+def frame_score_no_bonus(frame):
+    # for use in spares, strikes, and tenth frame scores
+    score = 0
+    for roll in frame:
+        if roll == "X":
+            return 10
+        if roll == "/":
+            return 10
+        else:
+            score += int(roll)
+    return score
+
 
 def bowling_point_calculator(bowl_str):
+    bowl_str = bowl_str.replace("-", "0")
     bowl_list = bowl_str.split()
     # ["1-","2-"...]
     total = 0
-    is_prev_spare = False
-    is_prev_strike = False
 
-    for i in bowl_list:
-        # i is frame
+    # calculate score for first 9 frames
+    for i in range(0, 9):
+        frame = bowl_list[i]
         current_frame_total = 0
-
-        #spare checking
-        if is_prev_spare and i[0] not in "-X":
-            total += int(i[0])
-            is_prev_spare = False
-        elif i[0] == "X": # would fail if bad input
-            is_prev_strike = True
-            total += 10
-            continue
-
-        for j in i:
-            # j is individual throw
-            if j not in "-/": # check
-                current_frame_total += int(j)
-
-        if i[1] == "/":
-            is_prev_spare = True
-            current_frame_total = 10
-        
-        if is_prev_strike: # account for strike points in current two throws
-            total += current_frame_total
-
+        # strike handling
+        if frame[0] == "X":
+            # add next two rolls
+            if bowl_list[i + 1] == "X":
+                total += 20 + frame_score_no_bonus(bowl_list[i + 2][0])
+            else:
+                total += 10 + frame_score_no_bonus(bowl_list[i + 1])
+            continue  # no second roll this frame
+        # spare handling
+        if frame[1] == "/":
+            total += 10 + frame_score_no_bonus(bowl_list[i + 1][0])
+            continue  # no need to look at first roll of frame
+        # non spare or strike
+        for roll in frame:
+            current_frame_total += int(roll)
         total += current_frame_total
+
+    # final frame handling
+    for frame in bowl_list[9:]:
+        if len(frame) > 2:  # spare
+            total += 10 + frame_score_no_bonus(frame[2:])
+        else:
+            total += frame_score_no_bonus(frame)
     return total
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,26 +1,58 @@
 # -- -- -- -- -- -- -- -- -- --
 from main.bowling import bowling_point_calculator
 
+
 def test_null():
-    assert bowling_point_calculator("-- "*10) == 0
+    assert bowling_point_calculator("-- " * 10) == 0
+
 
 def test_assert_one_nonzero_throw():
-    assert bowling_point_calculator("1- "+"-- "*9) == 1
+    assert bowling_point_calculator("1- " + "-- " * 9) == 1
+
 
 def test_multiple_nonzero_firstplace():
-    assert bowling_point_calculator("2- 4- -- 2- "+"-- "*6) == 8
+    assert bowling_point_calculator("2- 4- -- 2- " + "-- " * 6) == 8
+
 
 def test_multiple_nonzero_secondplace():
-    assert bowling_point_calculator("-2 -- -5 -9 "+"-- "*6) == 16
+    assert bowling_point_calculator("-2 -- -5 -9 " + "-- " * 6) == 16
+
 
 def test_multiple_nonzero():
-    assert bowling_point_calculator("-2 12 -5 -9 "+"-- "*6) == 19
+    assert bowling_point_calculator("-2 12 -5 -9 " + "-- " * 6) == 19
+
 
 def test_spare():
     assert bowling_point_calculator("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5-") == 140
 
+
 def test_strike_followedby_openframe():
-    assert bowling_point_calculator("X 13 ") == 18
+    assert bowling_point_calculator("X 13 " + "-- " * 8) == 18
+
+
+def test_strike_followedby_strike():
+    assert bowling_point_calculator("X X 24 " + "-- " * 7) == 44
+
+
+def test_tenthframe_spare():
+    assert bowling_point_calculator("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5") == 150
+
+
+def test_perfect_game():
+    assert bowling_point_calculator("X X X X X X X X X X X X") == 300
+
+
+def test_tenthframe_strike_spare():
+    assert bowling_point_calculator("-- " * 9 + "X 5/") == 20
+
+
+def test_tenthframe_spare_strike():
+    # two potential ways of writing this
+    one_way = bowling_point_calculator("-- " * 9 + "5/ X")
+    another_way = bowling_point_calculator("-- " * 9 + "5/X")
+    assert one_way == another_way == 20
+
+
 # Format
 # -    : miss
 # [1-9]: number of pins nocked down
